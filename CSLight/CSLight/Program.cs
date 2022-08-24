89a6e7dc88811ec98b55e76526f8725f845cbfc4
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-08-25 03:10:34 +0900

    Lesson_0
    Перемаенная

---
+++ CSLight/CSLight/Program.cs
@@ -0,0 +1,13 @@
+﻿string name;
+int age;
+float playermoney;
+string surname;
+bool isGrount;
+double playerdollar;
+int timeworld;
+float playerHeath;
+char worldSynbol;
+bool areYouAlive;
+
+
+    
