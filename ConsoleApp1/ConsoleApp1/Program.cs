fc7c8db7f07eef3701d9b795281ca17be1b7737f
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-08-25 18:17:12 +0900

    Lesson_1
    Анкета

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CSLight
+{
+    class Class1
+    {
+        static void Main()
+        {
+            string name;
+            string zodiacSign;
+            string areYouWork;
+
+            int age;
+            Console.WriteLine("Как вас завуд.");
+            name = Console.ReadLine();                
+            Console.WriteLine("Сколько вам лет.");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Какой ваш знак зодиака");
+            zodiacSign = Console.ReadLine();
+            Console.WriteLine("Где вы работаете?.");
+            areYouWork = Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Вас зовуд {name} вам {age} год.  Ваш знак Зодиака {zodiacSign} и работаете в {areYouWork}");
+        }
+    }
+}
