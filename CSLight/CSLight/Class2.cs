7c798f2d1dffba0523c85373a55324803ed42f1a
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-08-30 22:40:48 +0900

    Lesson

---
+++ CSLight/CSLight/Class2.cs
@@ -0,0 +1,16 @@
+﻿namespace CSLight
+{
+    class Class2
+    {
+        static void Main(string[] strings)
+        {
+            int x, y;
+            x = 52;
+            y = 3;
+
+            float result = x / y;
+
+            Console.WriteLine(result);
+        }
+    }
+}
