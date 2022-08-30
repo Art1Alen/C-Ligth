f85b53b37d9dea5e3a00226252afc62d8fc8a166
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-08-31 04:34:40 +0900

    Lesson

---
+++ CSLight/CSLight/Class3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    class Class3
+    {
+
+        static void Main(string[] strg)
+        {
+            string name = "Владимир";
+            string surname = "Пунтин";
+
+            Console.WriteLine($"Ваша Имя {name} {surname} Верно?");
+            Console.WriteLine("Если не верно то ведите новое Имя");
+            Console.ReadKey();
+            Console.WriteLine("Как вас завут?");
+            name = Console.ReadLine();
+            Console.WriteLine("Ваша фамилия?");
+            surname = Console.ReadLine();
+            Console.WriteLine($"Ваша Имя {name} {surname} Верно?");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
