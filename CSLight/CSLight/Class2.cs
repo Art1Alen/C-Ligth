fa1c2491bab190287193d3e45dc8bb74dbbcffd7
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-09-01 16:10:18 +0900

    Lesson
    исправил

--- CSLight/CSLight/Class2.cs
+++ CSLight/CSLight/Class2.cs
@@ -1,4 +1,10 @@
-﻿namespace CSLight
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
 {
     class Class2
     {
@@ -9,7 +15,7 @@
             int numberOfRows = numberOfPictures / numberOfPicturesInRow;
             int restOfThePaintings = numberOfPictures % numberOfPicturesInRow;
 
-            Console.WriteLine($"{RestOfThePaintings} Осталось картинок\n{numberOfRows} картин в ряд");
+            Console.WriteLine($"{restOfThePaintings} Осталось картинок\n{numberOfRows} картин в ряд");
         }
     }
 }
