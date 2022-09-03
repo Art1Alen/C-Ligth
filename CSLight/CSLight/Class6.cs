dd306c53846c29fd579a2b138e35c6634689c2d4
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-09-03 13:47:46 +0900

    Lesson
    cycle for

---
+++ CSLight/CSLight/Class6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Class6
+    {
+        static void Main(string[] strings)
+        {
+            int coint; 
+
+            Console.WriteLine("Введите количесво операци");
+            coint = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            for (int i = 0; i < coint; i++)
+            {             
+                Console.WriteLine(coint);
+            }
+        }
+    }
+}
