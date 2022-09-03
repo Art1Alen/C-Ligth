778526898397581e88dd1f64bfdbb6a7a5a64701
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-09-03 12:18:43 +0900

    Lesson
    Timer

---
+++ CSLight/CSLight/Class5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    class Class5
+    {
+        static void Main(string[] strg)
+        {
+            int grandmothers;
+
+            int timeOfReceipt = 10;
+            int hour;
+            int minute;
+
+            Console.WriteLine("Добро пожаловать в Поликлинике");
+            Console.WriteLine("Введите количество людей в очереди");
+
+            grandmothers = Convert.ToInt32(Console.ReadLine());
+
+            timeOfReceipt += grandmothers * 10 - 10;
+            hour = timeOfReceipt / 60;
+            minute = timeOfReceipt % 60;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hour} час и {minute} минут");
+        }
+    }
+}
