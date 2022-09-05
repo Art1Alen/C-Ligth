9f34ebd82fcc0c97b9dd27d61fa4bb3b5a874859
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-09-05 17:01:52 +0900

    Lesson

---
+++ CSLight/CSLight/Class7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Class7
+    {
+        static void Main()
+        {
+            int timerTicking = 1;
+
+            bool isExit;
+
+            if (isExit = true)
+            {
+                Console.WriteLine("Для выхода из таймера Введите exit");
+
+                for (int i = 0; i < timerTicking; i++)
+                {
+                    Console.WriteLine($"{timerTicking++}");                   
+                    string textExit = Console.ReadLine();
+                    Console.ReadLine();
+
+                    if (textExit == "exit")
+                    {
+                        isExit = false;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
