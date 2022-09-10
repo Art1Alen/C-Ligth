ba24588013528b41998cf7152563c2af75266398
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-09-10 11:21:44 +0900

    Lesson

---
+++ CSLight/CSLight/Class8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Class8
+    {
+        static void Main()
+        {
+            for (int i = 5; i < 100; i+=7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
