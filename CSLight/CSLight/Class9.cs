f6ef0dab3952ac553f7917516a7bbcbeb3303054
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-09-13 15:46:18 +0900

    Lesson

---
+++ CSLight/CSLight/Class9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Class9
+    {
+        static void Main()
+        {
+            Random random = new Random();
+
+            int minValue = 0;
+            int maxValue = 101;
+            int multiples1 = 3;
+            int multiples2 = 5;
+
+            int number = random.Next(minValue, maxValue);
+            
+            int summ = number;
+
+            Console.WriteLine(number);
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i % multiples1 == 0 || i % multiples2 == 0)
+                {
+                    summ += i;
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine(summ);
+        }
+    }
+}
