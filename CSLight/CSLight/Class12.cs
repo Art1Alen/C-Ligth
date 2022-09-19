2711373e04474000dbb892b8019ec6666ab8208b
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-09-20 08:58:34 +0900

    Lesson

--- CSLight/CSLight/Class12.cs
+++ CSLight/CSLight/Class12.cs
@@ -1,19 +1,40 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace CSLight
-//{
-//    internal class Class12
-//    {
-//        static void Main()
-//        {
-//            Console.WriteLine("Введите Имя?");
-//            string userName = Console.ReadLine();
-//            Console.WriteLine("Введите синвол");
-//            char userSymbol = Conver Console.ReadLine();
-//        }
-//    }
-//}
+namespace CSLight
+{
+    internal class Class12
+    {
+        static void Main()
+        {
+            string lateral = "";        
+
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Имя:  {name},длина имени: {name.Length}");
+
+            Console.Write("Введите символ: ");
+            string simbol = Console.ReadLine();
+
+            int additionToLength = 4;
+            string emptySpaceForSecondLine = " ";
+
+            Console.WriteLine($"Cимвол:  {simbol}");
+            Console.Clear();
+            Console.WriteLine("Ответь");
+
+            for (int i = 1; i <= (name.Length + additionToLength); i += 1)
+            {
+                lateral += simbol;
+            }
+
+            Console.WriteLine(lateral);
+            Console.WriteLine(lateral[0] + emptySpaceForSecondLine + name + emptySpaceForSecondLine + lateral[lateral.Length - 1]);
+            Console.WriteLine(lateral);
+        }
+    }
+}
