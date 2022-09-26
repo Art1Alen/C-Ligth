591d210c264ad1a41623b5f1e02e1143723f37a2
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-09-26 11:50:30 +0900

    Lesson

---
+++ CSLight/CSLight/Class15.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Class15
+    {
+        static void Main()
+        {
+            Random random = new Random();
+
+            int result = random.Next(0,100);
+            int doble = 2;
+      
+
+            if (result >= 0)
+            {
+                while (result != 0 && result != 1)
+                {
+                    result /= doble;
+                   Console.WriteLine(result);
+                }
+            }
+
+
+         
+        }
+    }
+}
