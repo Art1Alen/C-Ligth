80b8f29656ab8fa52b59aeb0e183ff27d2edbdb8
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-09-26 12:39:51 +0900

    Lesson

---
+++ CSLight/CSLight/Class16.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Class16
+    {
+        static void Main()
+        {
+            int symbol = 0;
+            int count = 0;
+            int result = 0;
+
+            Console.WriteLine("Введите строку из символо '(' и ')'");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '(')
+                {
+                    symbol++;
+                }
+                else if (text[i] == ')')
+                {
+                    if (i != text.Length - 1 && text[i + 1] != '(')
+                    {
+                        count++;
+                    }
+                    symbol--;
+                }
+                if (symbol < 0)
+                {
+                    break;
+                }
+                if (symbol == 0)
+                {
+                    result = count;
+                }
+            }
+            if (symbol == 0)
+            {
+                Console.WriteLine("Строка корректная " + text + "\n" + "Максимум глубина равняется: " + (result + 1));
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Не верная строка " + text);
+            }
+        }
+    }
+}
