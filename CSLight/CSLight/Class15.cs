05e9339e791c57c2694b4ca345a6f60f363e10f1
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-09-28 18:47:31 +0900

    Lesson

--- CSLight/CSLight/Class15.cs
+++ CSLight/CSLight/Class15.cs
@@ -13,11 +13,60 @@ namespace CSLight
             Random random = new Random();
 
             int numberRandom = random.Next(0, 100);
-            int doble = 2;
-            int result = numberRandom * doble;
+            int twoDegreeOne = 2;
+            int twoDegreeTwo = 4;
+            int twoDegreeThree = 8;
+            int twoDegreeFour = 16;
+            int twoDegreeFire = 32;
+            int twoDegreeSix = 64;
+            int twoDegreeSeven = 128;
 
-            Console.WriteLine($"{numberRandom} {result}");
+            string degree = "Кратные";
 
+            for (int i = 0; i < numberRandom; i++)
+            {
+                if (numberRandom <= twoDegreeOne)
+                {
+                    Console.WriteLine($"{numberRandom} {degree} 1 {twoDegreeOne}");
+                    break;
+                }
+
+                if (numberRandom <= twoDegreeTwo)
+                {
+                    Console.WriteLine($"{numberRandom} {degree} 2 {twoDegreeTwo}");
+                    break;
+                }
+
+                if (numberRandom <= twoDegreeThree)
+                {
+                    Console.WriteLine($"{numberRandom} {degree} 3 {twoDegreeThree}");
+                    break;
+                }
+
+                if (numberRandom <= twoDegreeFour)
+                {
+                    Console.WriteLine($"{numberRandom} {degree} 4 {twoDegreeFour}");
+                    break;
+                }
+
+                if (numberRandom <= twoDegreeFire)
+                {
+                    Console.WriteLine($"{numberRandom} {degree} 5 {twoDegreeFire}");
+                    break;
+                }
+
+                if (numberRandom <= twoDegreeSix)
+                {
+                    Console.WriteLine($"{numberRandom} {degree} 6 {twoDegreeSix}");
+                    break;
+                }
+
+                if (numberRandom <= twoDegreeSeven)
+                {
+                    Console.WriteLine($"{numberRandom} {degree} 7 {twoDegreeSeven}");
+                    break;
+                }
+            }
         }
     }
 }
