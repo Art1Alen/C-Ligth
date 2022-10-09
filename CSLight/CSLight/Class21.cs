0d0bb47ca0adf88af682862e6cfb1bec1d157aaf
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-10-09 10:55:54 +0900

    Lesson

---
+++ CSLight/CSLight/Class21.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Class21
+    {
+        static void Main()
+        {
+            const string CommandSum = "sum";
+            const string CommandExit = "exit";
+
+            int[] array = new int[0];
+
+            bool isWorking = true;
+
+            while (isWorking)
+            {
+                Console.Clear();
+                Console.WriteLine($"Введите число или команду ({CommandSum} - сложить числа, {CommandExit} - выход):");
+
+                string inputUser = Console.ReadLine().ToLower();
+
+                switch (inputUser)
+                {
+                    case CommandSum:
+
+                        int sum = 0;
+
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            sum += array[i];
+                        }
+
+                        Console.WriteLine($"Сумма чисел: {sum}");
+                        Console.ReadLine();
+
+                        break;
+
+                    case CommandExit:
+
+                        isWorking = false;
+
+                        break;
+                         
+                }
+            }
+        }
+    }
+}
