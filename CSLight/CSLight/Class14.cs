66f701ec34373dfbd569dd9d060f357a526eb947
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-10-18 10:56:31 +0900

    Lesson

--- CSLight/CSLight/Class14.cs
+++ CSLight/CSLight/Class14.cs
@@ -1,33 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSLight
+﻿namespace CSLight
 {
     internal class Class14
     {
         static void Main()
         {
-            int maxNumber = 27;
-            int minNumber = 1;
-            int number = 10;
+            int minNumberN = 1;
+            int maxNumberN = 27;
+            int threeDigit = 999;
 
-            for (int i = minNumber; i <= maxNumber; i++)
+            Random random = new Random();
+            int numberN = random.Next(minNumberN, maxNumberN);
+
+            for (int i = 0; i < threeDigit; i += numberN)
             {
-                for (int j = minNumber; j < number; j++)
+                if (i != numberN)
                 {
-                    for (int k = 0; k < number; k++)
-                    {
-                        for (int l = 0; l < number; l++)
-                        {
-                            if (j + k + l == i)
-                            {
-                                Console.WriteLine($" {i} || {j}{k}{l}");
-                            }
-                        }
-                    }
+                    Console.WriteLine(i);
                 }
             }
         }
