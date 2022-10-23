d7204a72abaed39a2d784aa494d87185861101d6
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-10-23 16:05:30 +0900

    Lesson

--- Lesson C#/Class14.cs
+++ Lesson C#/Class14.cs
@@ -8,14 +8,14 @@
             int maxNumber = 28;
             int cardboardNumber = 999;
             int count = 0;
-            int threeDigitNumber = 99;
+            int three_DigitNumber = 99;
 
             Random random = new Random();
             int numberRandom = random.Next(minNumber, maxNumber);
 
             for (int i = 0; i <= cardboardNumber; i += numberRandom)
             {
-                if (i > threeDigitNumber)
+                if (i > three_DigitNumber)
                     count++;
             }
 
