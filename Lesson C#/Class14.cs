f96e9da05399ef3c817b0bacc19de4f26a45c451
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-10-25 09:46:28 +0900

    Lesson

--- Lesson C#/Class14.cs
+++ Lesson C#/Class14.cs
@@ -8,14 +8,14 @@
             int maxNumber = 28;
             int cardboardNumber = 999;
             int count = 0;
-            int moreThanThreeDigits = 99;
+            int multiplicityOfNumbers = 99;
 
             Random random = new Random();
             int numberRandom = random.Next(minNumber, maxNumber);
 
             for (int i = 0; i <= cardboardNumber; i += numberRandom)
             {
-                if (i > moreThanThreeDigits)
+                if (i > multiplicityOfNumbers)
                     count++;
             }
 
