cf807b95b9c705d70c0edd0f86989101d7461a5b
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-10-25 11:14:42 +0900

    Lesson

--- Lesson C#/Class14.cs
+++ Lesson C#/Class14.cs
@@ -1,25 +1,25 @@
-﻿namespace CSLight
-{
-    internal class Class14
-    {
-        static void Main()
-        {
-            int minNumber = 1;
-            int maxNumber = 28;
-            int cardboardNumber = 999;
-            int count = 0;
-            int multiplicityOfNumbers = 99;
+﻿//namespace CSLight
+//{
+//    internal class Class14
+//    {
+//        static void Main()
+//        {
+//            int minNumber = 1;
+//            int maxNumber = 28;
+//            int cardboardNumber = 999;
+//            int count = 0;
+//            int multiplicityOfNumbers = 99;
 
-            Random random = new Random();
-            int numberRandom = random.Next(minNumber, maxNumber);
+//            Random random = new Random();
+//            int numberRandom = random.Next(minNumber, maxNumber);
 
-            for (int i = 0; i <= cardboardNumber; i += numberRandom)
-            {
-                if (i > multiplicityOfNumbers)
-                    count++;
-            }
+//            for (int i = 0; i <= cardboardNumber; i += numberRandom)
+//            {
+//                if (i > multiplicityOfNumbers)
+//                    count++;
+//            }
 
-            Console.WriteLine(count);
-        }
-    }
-}
+//            Console.WriteLine(count);
+//        }
+//    }
+//}
--- Lesson C#/Class20.cs
+++ Lesson C#/Class20.cs
@@ -2,50 +2,37 @@
 {
     internal class Class20
     {
+
+
+
         static void Main()
         {
             Random random = new Random();
 
-            string spaces = " ";
-            string localMaximaArray = " ";
-            string doubleQuotes = " ";
-
             int minNumber = 1;
             int maxNumber = 10;
+            int localMaxNumber = 0;
+
+            string space = " ";
 
             int[] array = new int[30];
 
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 1; i < array.Length - 1; i++)
             {
                 array[i] = random.Next(minNumber, maxNumber);
-
-                spaces += Convert.ToString(array[i] + " ");
-
-                if (i == array.GetLength(0) - 1)
-                {
-                    for (int k = 1; k < array.GetLength(0) - 1; k++)
-                    {
-
-                        if (array[k] > array[k + 1] && array[k] > array[k - 1])
-                        {
-                            localMaximaArray += array[k] + " ";
-                        }
-                    }
-                }
-
-                if ((i == array.GetLength(0) - 1) && (array[0] > array[1]))
-                {
-                    localMaximaArray += array[0] + " ";
-                }
-
-                if ((i == array.GetLength(0) - 1) && (array[array.GetLength(0) - 1] > array[array.GetLength(0) - 2]))
-                {
-                    localMaximaArray += "" + array[array.GetLength(0) - 1];
-                }
+                Console.Write(array[i] + space);
             }
 
-            Console.WriteLine($"Исходный массив{spaces}");
-            Console.WriteLine($"Локальные максимумы: {localMaximaArray}");
+            Console.WriteLine();
+            Console.WriteLine("Локальный максимум");
+
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                if (array[i - 1] < array[i] && array[i] > array[i + 1])
+                {
+                    Console.Write(array[i] + space);
+                }
+            }
         }
     }
 }
