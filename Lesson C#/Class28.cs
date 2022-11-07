46aa5011131e84c0823376f136a007c2e6232157
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-11-07 18:53:12 +0900

    Lesson C#

---
+++ Lesson C#/Class28.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Class28
+    {
+        static void Main()
+        {
+            string userInput;
+
+            MessageOutput("Привет Введите любое число");
+
+            int conversion;
+
+            userInput = Console.ReadLine();
+
+            bool result = int.TryParse(userInput, out conversion);
+
+            if (result)
+            {
+                MessageOutput($"Преобразование прошло успешно. Число: {conversion}");
+            }
+            else
+            {
+                MessageOutput("Преобразование завершилось неудачно");
+
+            }
+        }
+
+        static void MessageOutput(string text)
+        {
+            Console.WriteLine(text);
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
+
+
