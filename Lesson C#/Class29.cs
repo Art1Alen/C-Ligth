d35b2406a57865c9f9eef41c5a85866aa82f87e4
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-11-14 10:56:46 +0900

    Lesson C#
    Сделать игровую карту с помощью двумерного массива. Сделать функцию рисования карты. Помимо этого, дать пользователю возможность перемещаться по карте и взаимодействовать с элементами (например пользователь не может пройти сквозь стену)

---
+++ Lesson C#/Class29.cs
@@ -0,0 +1,101 @@
+﻿namespace CSLight
+{
+    internal class Class29
+    {
+        public static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            bool isPlaing = true;
+
+            char[,] map = new char[,]
+                {
+                {'#','#','#','#','#','#'},
+                {'#',' ','#',' ',' ','#'},
+                {'#',' ',' ','#',' ','#'},
+                {'#',' ',' ',' ',' ','#'},
+                {'#',' ','#',' ','#','#'},
+                {'#',' ','#',' ',' ','#'},
+                {'#','#','#','#','#','#'}
+                };
+            int positionUserX = 1;
+            int positionUserY = 1;
+            int directionX = 0;
+            int directionY = 0;
+
+            DrawingMap(map);
+            DrawUser(positionUserY, positionUserX);
+
+            while (isPlaing)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                ChangeDirection(key, ref directionX, ref directionY);
+
+                if (map[positionUserX + directionX, positionUserY + directionY] != '#')
+                {
+                    MoveUser(ref positionUserX, ref positionUserY, directionX, directionY);
+                }
+
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    isPlaing = false;
+                }
+            }
+        }
+
+        static void DrawingMap(char[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void DrawUser(int positionY, int positionX)
+        {
+            Console.SetCursorPosition(positionY, positionX);
+            Console.Write('@');
+        }
+
+        static void MoveUser(ref int positionX, ref int positionY, int directionX, int directionY)
+        {
+            Console.SetCursorPosition(positionY, positionX);
+            Console.Write(" ");
+
+            positionX += directionX;
+            positionY += directionY;
+
+            Console.SetCursorPosition(positionY, positionX);
+            Console.Write('@');
+        }
+
+        static void ChangeDirection(ConsoleKeyInfo key, ref int directionX, ref int directionY)
+        {
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    directionX = -1;
+                    directionY = 0;
+                    break;
+                case ConsoleKey.DownArrow:
+                    directionX = 1;
+                    directionY = 0;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    directionX = 0;
+                    directionY = -1;
+                    break;
+                case ConsoleKey.RightArrow:
+                    directionX = 0;
+                    directionY = 1;
+                    break;
+            }
+        }
+    }
+}
+
