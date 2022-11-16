2ece1d1eadf3c3d2aa6b73f32d3023abceb01065
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-11-16 17:30:46 +0900

    Lesson C#
    Разработайте функцию, которая рисует некий бар (Healthbar, Manabar) в определённой позиции. Она также принимает некий закрашенный процент.
    
    При 40% бар выглядит так:
    
    [####______]

--- Lesson C#/Class27.cs
+++ Lesson C#/Class27.cs
@@ -24,7 +24,7 @@
             {
                 while (isOpenApp)
                 {
-                    DrawingHealthBar(health, maxHealth, ConsoleColor.Red);
+                    HealthBar(health, maxHealth, ConsoleColor.Red);
 
                     Console.SetCursorPosition(0, 5);
 
@@ -38,7 +38,7 @@
             }
         }
 
-        static void DrawingHealthBar(int value, int maxValue, ConsoleColor color, char symbol = '#')
+        static void HealthBar(int value, int maxValue, ConsoleColor color, char symbol = '#')
         {
             ConsoleColor defaultColor = Console.BackgroundColor;
 
