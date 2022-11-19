4ed4b81a0cfc65cefbd5c0665c407bfd2a3f14a3
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-11-19 18:39:20 +0900

    Lesson C#
    У вас есть множество целых чисел. Каждое целое число - это сумма покупки.
    Вам нужно обслуживать клиентов до тех пор, пока очередь не станет пуста.
    После каждого обслуженного клиента деньги нужно добавлять на наш счёт и выводить его в консоль.
    После обслуживания каждого клиента программа ожидает нажатия любой клавиши, после чего затирает консоль и по новой выводит всю информацию, только уже со следующим клиентом

---
+++ Lesson C#/Class32.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Class32
+    {
+        static void Main()
+        {          
+                StoreQueue();                     
+        }
+
+        static void StoreQueue()
+        {
+            Random random = new Random();
+
+            Queue<int> buyers = new Queue<int>();
+
+            int maxNumber = 100;
+            int minNumber = 25;
+            int cashAmount = 0;
+
+            bool isExit = false;
+
+            buyers.Enqueue(random.Next(minNumber, maxNumber));
+            buyers.Enqueue(random.Next(minNumber, maxNumber));
+            buyers.Enqueue(random.Next(minNumber, maxNumber));
+            buyers.Enqueue(random.Next(minNumber, maxNumber));
+
+            do
+            {
+                foreach (var buyer in buyers)
+                {
+                    Console.WriteLine(buyer);
+                }
+
+                Console.WriteLine($"на Кассе денег {cashAmount += buyers.Peek()}");
+                Console.WriteLine($"На кассе {buyers.Dequeue()}");
+
+                Console.ReadKey();
+                Console.Clear();
+
+            }while (isExit);                                   
+        }
+    }
+}
