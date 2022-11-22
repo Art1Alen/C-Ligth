aad95ad995f3fa03ceec079ed870a133f6d421f0
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-11-22 19:18:59 +0900

    Lesson C#
    В массивах вы выполняли задание "Динамический массив"
    Используя всё изученное, напишите улучшенную версию динамического массива(не обязательно брать своё старое решение)
    Задание нужно, чтобы вы освоились с List и прощупали его преимущество.
    Проверка на ввод числа обязательна.
    
    Пользователь вводит числа, и программа их запоминает.
    Как только пользователь введёт команду sum, программа выведет сумму всех веденных чисел.
    
    Выход из программы должен происходить только в том случае, если пользователь введет команду exit.

---
+++ Lesson C#/Class33.cs
@@ -0,0 +1,61 @@
+﻿namespace CSLight
+{
+    internal class Class33
+    {
+        const string CommandSum = "sum";
+        const string CommandExit = "exit";
+       
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>(); 
+
+            bool isWorking = true;
+
+            while (isWorking == true)
+            {
+                Console.WriteLine($"Введите число");
+                string userInput = Console.ReadLine().ToLower();
+
+                switch (userInput)
+                {
+                    case CommandSum:
+                        Sum(numbers);
+                        break;
+
+                        case CommandExit:
+                        isWorking = false;
+                        break;
+
+                    default:
+                        AddNumber(userInput, numbers);
+                        break;
+                }
+            }
+        }
+     
+        static void Sum(List<int> numbers)
+        {
+            int sum = 0;
+            foreach (int numberInList in numbers)
+            {
+                sum += numberInList;
+            }
+
+            Console.WriteLine($"\nСумма введёных чисел: {sum}\n");
+        }
+
+        static void AddNumber(string userInput, List<int> numbers)
+        {
+            bool isNumber = int.TryParse(userInput, out int number);
+
+            if (isNumber == true)
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("\nНекорректный ввод\n");
+            }
+        }
+    }
+}
