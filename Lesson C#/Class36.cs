95ca04756078ab934a6e13409eb6f257faa56a7c
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-12-01 19:12:42 +0900

    Lesson C# ООП
    Создать класс игрока, с полями, содержащими информацию об игроке и методом, который выводит информацию на экран.
    
    В классе обязательно должен быть конструктор

---
+++ Lesson C#/Class36.cs
@@ -0,0 +1,30 @@
+﻿namespace CSLight
+{
+    internal class Class36
+    {
+        static void Main()
+        {
+            Player player = new Player(100, 25, 45);
+            player.PlayerStatistics();
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+        private int _armor;
+        private int _damage;
+       
+        public Player(int health, int armor, int damage)
+        {
+            _health = health;
+            _armor = armor;
+            _damage = damage;
+        }
+
+        public void PlayerStatistics()
+        {
+            Console.WriteLine($"Здоровя {_health}\nБроня {_armor}\nУрон {_damage}");
+        }
+    }
+}
