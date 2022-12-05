71d8ef1f16c23f083eb90a7064dabff48876f62b
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2022-12-05 18:57:22 +0900

    Lesson C# ООП
    Реализовать базу данных игроков и методы для работы с ней.
    У игрока может быть уникальный номер, ник, уровень, флаг – забанен ли он(флаг - bool).
    Реализовать возможность добавления игрока, бана игрока по уникальный номеру, разбана игрока по уникальный номеру и удаление игрока.
    Создание самой БД не требуется, задание выполняется инструментами, которые вы уже изучили в рамках курса. Но нужен класс, который содержит игроков и её можно назвать "База данных".

---
+++ Lesson C#/Class38.cs
@@ -0,0 +1,151 @@
+﻿namespace CSLight
+{
+    internal class Class38
+    {
+        public static void Main()
+        {
+            const string CommandAddPlayer = "1";
+            const string CommandBanPlayer = "2";
+            const string CommandUnbanPlayer = "3";
+            const string CommandDeletePlayer = "4";
+            const string CommandShowPlayers = "5";
+            const string CommandExit = "exit";
+
+            DataBase dataBase = new DataBase();
+          
+            bool isWorking = true;
+
+            while (isWorking)
+            {
+                Console.WriteLine($"{CommandAddPlayer} - добавить игрока в базуданных. " +
+                $"\n{CommandBanPlayer} - забанить игрока по Id," +
+                $"\n{CommandUnbanPlayer} - разбанить игрока," +
+                $"\n{CommandDeletePlayer} - удалить игрока," +
+                $"\n{CommandShowPlayers} - показать всех игроков," +
+                $"\n{CommandExit} - выход.");
+
+                Console.WriteLine("Выберите пункт меню ");
+                string inputUser = Console.ReadLine();
+
+                switch (inputUser)
+                {
+                    case CommandAddPlayer:
+                        dataBase.CreatePlayer();
+                        break;
+                    case CommandBanPlayer:
+                        dataBase.BanPlayer();
+                        break;
+                    case CommandUnbanPlayer:
+                        dataBase.UnBanPlayer();
+                        break;
+                    case CommandDeletePlayer:
+                        dataBase.RemovePlayer();
+                        break;
+                    case CommandShowPlayers:
+                        dataBase.ShowPlayers();
+                        break;
+                    case CommandExit:
+                        isWorking = false;
+                        break;
+                    default:
+                        Console.WriteLine("Неправильно выбран пункт меню! ");
+                        break;
+                }
+                Console.Clear();
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class DataBase
+    {
+        public List<Player> players = new List<Player>();
+
+        public void CreatePlayer()
+        {
+            int newId = players.Count() + 1;
+            Console.WriteLine("Введите Name");
+
+            string newName = Console.ReadLine();
+            Console.WriteLine("Введите Level");
+
+            int newLevel = int.Parse(Console.ReadLine());
+            Console.WriteLine("Статус VAC бан игрока  - (true)  или нет - (false)");
+
+            bool newIsBanned = Convert.ToBoolean(Console.ReadLine());
+            players.Add(new Player(newId, newName, newLevel, newIsBanned));
+        }
+
+        public void RemovePlayer()
+        {
+            Console.WriteLine("Для Удаление Требуется Ввести id номер игрока");
+            int selectedId = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (players[i].Id == selectedId)
+                {
+                    players.RemoveAt(i);
+                }
+            }
+        }
+
+        public void ShowPlayers()
+        {
+            foreach (var player in players)
+            {
+                player.ShowInfo();
+            }
+            Console.ReadKey();
+        }
+
+        public void BanPlayer()
+        {
+            Console.WriteLine("Требуется id номер Игрока бля VAC бана");
+            int selectedId = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (players[i].Id == selectedId)
+                {
+                    players[i].IsBanned = true;
+                }
+            }
+        }
+
+        public void UnBanPlayer()
+        {
+            Console.WriteLine("Требуется id номер Игрока бля разбана VAC");
+            int selectedId = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (players[i].Id == selectedId)
+                {
+                    players[i].IsBanned = false;
+                }
+            }
+        }
+    }
+
+    class Player
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Level { get; private set; }
+        public bool IsBanned { get; set; }
+        public Player(int id, string name, int level, bool isBanned)
+        {
+            Id = id;
+            Name = name;
+            Level = level;
+            IsBanned = isBanned;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Уникальный номер: {Id}, Имя: {Name}, Уровень персонажа: {Level}, Забанен: {IsBanned}");
+        }
+    }
+}
+
