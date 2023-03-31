2ff3f2c4b81156cec65c01fd186d46f6f101e010
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2023-03-31 19:25:42 +0900

    Lesson C# ООП Zoo
    Пользователь запускает приложение и перед ним находится меню, в котором он может выбрать, к какому вольеру подойти. При приближении к вольеру, пользователю выводится информация о том, что это за вольер, сколько животных там обитает, их пол и какой звук издает животное.
    Вольеров в зоопарке может быть много, в решении нужно создать минимум 4 вольера.

--- Lesson C#/Class47.cs
+++ Lesson C#/Class47.cs
@@ -1,217 +1,217 @@
-﻿namespace CSLight
-{
-    internal class Class47
-    {
-        static void Main(string[] args)
-        {
-            const string CommandAddFish = "1";
-            const string CommandRemoveFish = "2";
-            const string CommandExit = "3";
+﻿//namespace CSLight
+//{
+//    internal class Class47
+//    {
+//        static void Main(string[] args)
+//        {
+//            const string CommandAddFish = "1";
+//            const string CommandRemoveFish = "2";
+//            const string CommandExit = "3";
 
-            Aquarium aquarium = new Aquarium();
+//            Aquarium aquarium = new Aquarium();
 
-            bool isWork = true;
+//            bool isWork = true;
 
-            while (isWork)
-            {
-                aquarium.ShowInfo();
-                Console.WriteLine($"\nАквариум \n{CommandAddFish}. Добавить Рыбу  \n{CommandRemoveFish}. Убрать рыбу из аквариума\n{CommandExit}. Выход. ");
-                Console.WriteLine("Выберите Комманду");
+//            while (isWork)
+//            {
+//                aquarium.ShowInfo();
+//                Console.WriteLine($"\nАквариум \n{CommandAddFish}. Добавить Рыбу  \n{CommandRemoveFish}. Убрать рыбу из аквариума\n{CommandExit}. Выход. ");
+//                Console.WriteLine("Выберите Комманду");
 
-                string userInput = Console.ReadLine();
+//                string userInput = Console.ReadLine();
 
-                switch (userInput)
-                {
-                    case CommandAddFish:
-                        aquarium.AddFish();
-                        break;
+//                switch (userInput)
+//                {
+//                    case CommandAddFish:
+//                        aquarium.AddFish();
+//                        break;
 
-                    case CommandRemoveFish:
-                        aquarium.RemoveFish();
-                        break;
+//                    case CommandRemoveFish:
+//                        aquarium.RemoveFish();
+//                        break;
 
-                    case CommandExit:
-                        Console.WriteLine("Выход");
-                        isWork = false;
-                        break;
+//                    case CommandExit:
+//                        Console.WriteLine("Выход");
+//                        isWork = false;
+//                        break;
 
-                    default:
-                        Console.WriteLine("Ошибка");
-                        break;
-                }
+//                    default:
+//                        Console.WriteLine("Ошибка");
+//                        break;
+//                }
 
-                aquarium.SkipTime();
-                aquarium.RemoveDeadFish();
-                Console.Clear();
-            }
-        }
-    }
+//                aquarium.SkipTime();
+//                aquarium.RemoveDeadFish();
+//                Console.Clear();
+//            }
+//        }
+//    }
 
-    class Aquarium
-    {
-        private int _maxCountOfFish = 5;
-        private List<Fishes> _fishes = new List<Fishes>();
+//    class Aquarium
+//    {
+//        private int _maxCountOfFish = 5;
+//        private List<Fishes> _fishes = new List<Fishes>();
 
-        public Aquarium()
-        {
-            _fishes.Add(new Fishes(2, "Толстая Камбола"));
-        }
+//        public Aquarium()
+//        {
+//            _fishes.Add(new Fishes(2, "Толстая Камбола"));
+//        }
 
-        public void AddFish()
-        {
-            string name;
-            int age;
+//        public void AddFish()
+//        {
+//            string name;
+//            int age;
 
-            if (_maxCountOfFish <= _fishes.Count)
-            {
-                Console.Clear();
-                Console.WriteLine("В аквариуме нету места");
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.Write("Введите имя рыбки: ");
-                name = GetInputName();
+//            if (_maxCountOfFish <= _fishes.Count)
+//            {
+//                Console.Clear();
+//                Console.WriteLine("В аквариуме нету места");
+//                Console.ReadKey();
+//            }
+//            else
+//            {
+//                Console.Write("Введите имя рыбки: ");
+//                name = GetInputName();
 
-                Console.Write("Введите возраст рыбы: ");
-                age = GetInputAge();
+//                Console.Write("Введите возраст рыбы: ");
+//                age = GetInputAge();
 
-                if (name == null || age <= 0)
-                {
-                    Console.WriteLine("Ошибка. Введены не коректные данные");
-                }
-                else
-                {
-                    _fishes.Add(new Fishes(age, name));
-                }
-            }
-        }
+//                if (name == null || age <= 0)
+//                {
+//                    Console.WriteLine("Ошибка. Введены не коректные данные");
+//                }
+//                else
+//                {
+//                    _fishes.Add(new Fishes(age, name));
+//                }
+//            }
+//        }
 
-        public void RemoveFish()
-        {
-            if (TryGetFish(out Fishes fish))
-            {
-                _fishes.Remove(fish);
-            }
-        }
+//        public void RemoveFish()
+//        {
+//            if (TryGetFish(out Fishes fish))
+//            {
+//                _fishes.Remove(fish);
+//            }
+//        }
 
-        public void ShowInfo()
-        {
-            int numberFish = 1;
-            Console.WriteLine($"\nКоличество Рыб в Аквариуме - {_fishes.Count}");
+//        public void ShowInfo()
+//        {
+//            int numberFish = 1;
+//            Console.WriteLine($"\nКоличество Рыб в Аквариуме - {_fishes.Count}");
 
-            foreach (Fishes fish in _fishes)
-            {
-                Console.WriteLine($"{numberFish++}. {fish.Name}, возраст {fish.Age - 1}");
-            }
-        }
+//            foreach (Fishes fish in _fishes)
+//            {
+//                Console.WriteLine($"{numberFish++}. {fish.Name}, возраст {fish.Age - 1}");
+//            }
+//        }
 
-        public void RemoveDeadFish()
-        {
-            for (int i = _fishes.Count - 1; i >= 0; i--)
-            {
-                if (_fishes[i].IsALive == false)
-                {
-                    Fishes fish = _fishes[i];
-                    _fishes.Remove(fish);
-                }
-            }
-        }
+//        public void RemoveDeadFish()
+//        {
+//            for (int i = _fishes.Count - 1; i >= 0; i--)
+//            {
+//                if (_fishes[i].IsALive == false)
+//                {
+//                    Fishes fish = _fishes[i];
+//                    _fishes.Remove(fish);
+//                }
+//            }
+//        }
 
-        public void SkipTime()
-        {
-            foreach (Fishes fish in _fishes)
-            {
-                fish.Live();
-            }
-        }
+//        public void SkipTime()
+//        {
+//            foreach (Fishes fish in _fishes)
+//            {
+//                fish.Live();
+//            }
+//        }
 
-        private bool TryGetFish(out Fishes fish)
-        {
-            Console.Write("Введите номер рыбы: ");
-            bool isNumber = int.TryParse(Console.ReadLine().ToLower(), out int inputNumberFish);
+//        private bool TryGetFish(out Fishes fish)
+//        {
+//            Console.Write("Введите номер рыбы: ");
+//            bool isNumber = int.TryParse(Console.ReadLine().ToLower(), out int inputNumberFish);
 
-            if (isNumber == false)
-            {
-                Console.WriteLine("Ошибка! Вы ввели не коректные данные.");
-                fish = null;
-                return false;
-            }
-            else if (inputNumberFish > 0 && inputNumberFish - 1 < _fishes.Count)
-            {
-                fish = _fishes[inputNumberFish - 1];
-                Console.WriteLine("Достали рыбу");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("Нет такой рыбы");
-                fish = null;
-                return false;
-            }
-        }
+//            if (isNumber == false)
+//            {
+//                Console.WriteLine("Ошибка! Вы ввели не коректные данные.");
+//                fish = null;
+//                return false;
+//            }
+//            else if (inputNumberFish > 0 && inputNumberFish - 1 < _fishes.Count)
+//            {
+//                fish = _fishes[inputNumberFish - 1];
+//                Console.WriteLine("Достали рыбу");
+//                return true;
+//            }
+//            else
+//            {
+//                Console.WriteLine("Нет такой рыбы");
+//                fish = null;
+//                return false;
+//            }
+//        }
 
-        private int GetInputAge()
-        {
-            int maximumLength = 1;
-            int maximumAge = 9;
-            bool isNumber = int.TryParse(Console.ReadLine(), out int age);
+//        private int GetInputAge()
+//        {
+//            int maximumLength = 1;
+//            int maximumAge = 9;
+//            bool isNumber = int.TryParse(Console.ReadLine(), out int age);
 
-            if (isNumber == true && age >= maximumLength && age <= maximumAge)
-            {
-                Console.WriteLine("Дейстие выполнено.");
-                return age;
-            }
-            else
-            {
-                Console.WriteLine("Введены некоретные данный");
-                return age;
-            }
-        }
+//            if (isNumber == true && age >= maximumLength && age <= maximumAge)
+//            {
+//                Console.WriteLine("Дейстие выполнено.");
+//                return age;
+//            }
+//            else
+//            {
+//                Console.WriteLine("Введены некоретные данный");
+//                return age;
+//            }
+//        }
 
-        private string GetInputName()
-        {
-            string name = Console.ReadLine();
+//        private string GetInputName()
+//        {
+//            string name = Console.ReadLine();
 
-            foreach (char symbol in name)
-            {
-                if (char.IsLetter(symbol) == false)
-                {
-                    Console.WriteLine("Введены некоретные данный поторите попытку.");
-                    return null;
-                }
-            }
+//            foreach (char symbol in name)
+//            {
+//                if (char.IsLetter(symbol) == false)
+//                {
+//                    Console.WriteLine("Введены некоретные данный поторите попытку.");
+//                    return null;
+//                }
+//            }
 
-            return name;
-        }
+//            return name;
+//        }
 
-    }
+//    }
 
-    class Fishes
-    {
-        private int _lethalAge = 10;
+//    class Fishes
+//    {
+//        private int _lethalAge = 10;
 
-        public Fishes(int age, string name)
-        {
-            Age = age;
-            Name = name;
-        }
+//        public Fishes(int age, string name)
+//        {
+//            Age = age;
+//            Name = name;
+//        }
 
-        public int Age { get; private set; }
-        public string Name { get; private set; }
-        public bool IsALive
-        {
-            get
-            {
-                return Age < _lethalAge;
-            }
-            private set { }
-        }
+//        public int Age { get; private set; }
+//        public string Name { get; private set; }
+//        public bool IsALive
+//        {
+//            get
+//            {
+//                return Age < _lethalAge;
+//            }
+//            private set { }
+//        }
 
-        public void Live()
-        {
-            Age++;
-        }
-    }
-}
+//        public void Live()
+//        {
+//            Age++;
+//        }
+//    }
+//}
 
---
+++ Lesson C#/Class48.cs
@@ -0,0 +1,166 @@
+﻿namespace CSLight
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zoo zoo = new Zoo();
+            zoo.StartExcursion();
+            Console.ReadLine();
+        }
+    }
+
+    class Zoo
+    {
+        const string CommandStart = "1";
+        const string CommandExit = "2";
+        private List<Aviary> _aviaries = new List<Aviary>();
+
+        public void StartExcursion()
+        {
+            CreativeAviary(5);
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.WriteLine($"Добро пожаловать в зоопар.\nДля налало нажмите {CommandStart}\nВыход из зоопарка {CommandExit}");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case CommandStart:
+                        ShowAviary();
+                        break;
+
+                    case CommandExit:
+                        isWork = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Не верный ввод");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private void CreativeAviary(int numberOfAviary)
+        {
+            for (int i = 0; i < numberOfAviary; i++)
+            {
+                _aviaries.Add(new Aviary());
+            }
+        }
+
+        private void ShowAviary()
+        {           
+            Console.Write("На какой вольер вы хотите посмотреть?: Введите номер от 1 до 4\n");
+            bool isNumber = int.TryParse(Console.ReadLine(), out int inputNumberAviary);
+
+            if (isNumber == false)
+            {
+                Console.WriteLine("Ошибка! Вы ввели не коректные данные.");
+            }
+            else if (inputNumberAviary > 0 && inputNumberAviary < _aviaries.Count)
+            {
+                _aviaries[inputNumberAviary - 1].ShowAnimal();
+            }
+            else
+            {
+                Console.WriteLine("Вальера с таким номером в зоопарке нету.");
+            }
+        }
+    }
+
+    class Aviary : Zoo
+    {
+        private Random _random = new Random();
+
+        private Dictionary<int, Animal> _animals = new Dictionary<int, Animal>();
+        private string[] _listAnimals = new string[] { "Тигр", "Собака", "Попугай", "Кот", "Крокодил" };
+
+        public Aviary()
+        {
+            CreativeAnimal(5);
+        }
+
+        public void ShowAnimal()
+        {
+            Console.WriteLine($"\nКоличиство животных в вольере - {_animals.Count}");
+
+            foreach (var animal in _animals)
+            {
+                Console.WriteLine($"{animal.Value.Name}. Пол - {animal.Value.Gender}. Голос - {animal.Value.Voice}");
+            }
+        }
+
+        private void CreativeAnimal(int numberOfAnimals)
+        {
+            int animalId = _random.Next(0, _listAnimals.Length);
+
+            for (int i = 0; i < numberOfAnimals; i++)
+            {
+                _animals.Add(i, GetAnimal(animalId));
+            }
+        }
+
+        private Animal GetAnimal(int animalId)
+        {
+            switch (_listAnimals[animalId])
+            {
+                case "Тигр":
+                    return new Animal("Тигр", "Р-р-р");
+
+                case "Собака":
+                    return new Animal("Собаки", "Гаф");
+
+                case "Попугай":
+                    return new Animal("Попугай", "Кто - там");
+
+                case "Кот":
+                    return new Animal("Кот", "Мяу Мяу");
+
+                case "Крокодил":
+                    return new Animal("Крокодил", "Клоц-клоц");
+            }
+
+            return null;
+        }
+    }
+
+    class Animal
+    {
+        private Random _random = new Random();
+
+        public Animal(string name, string voice)
+        {
+            Name = name;
+            Gender = GetGenderAnimal();
+            Voice = voice;
+        }
+
+        public string Name { get; private set; }
+        public string Gender { get; private set; }
+        public string Voice { get; private set; }
+
+        private string GetGenderAnimal()
+        {
+            int minimumNumberGender = 0;
+            int maximumNumberGender = 2;
+
+            int gender = _random.Next(minimumNumberGender, maximumNumberGender);
+
+            if (gender == 1)
+            {
+                return "Самец";
+            }
+            else
+            {
+                return "Самка";
+            }
+        }
+    }
+}
