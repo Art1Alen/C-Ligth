26bf0eab01ebad44414511794aabf22e0112cfff
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2023-04-26 18:01:09 +0900

    Update Class48.cs

--- Lesson C#/Class48.cs
+++ Lesson C#/Class48.cs
@@ -1,294 +1,196 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSLight
+﻿namespace CSLight
 {
-    internal class Class49
+    internal class Program
     {
-        internal class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                CarService carService = new CarService();
-                carService.Work();
-            }
+            Zoo zoo = new Zoo();
+            zoo.Work();
+            Console.ReadLine();
+        }
+    }
+
+    class Zoo
+    {
+        private const string CommandStart = "1";
+        private const string CommandExit = "2";
+
+        private static Random _random = new Random();
+
+        private List<Aviary> _aviaries = new List<Aviary>();
+        private List<Animal> _animals = new List<Animal>();
+
+        public Zoo()
+        {
+            FillAnimas();
+            CreateAviary();
         }
 
-        class CarService
+        public void Work()
         {
-            private Storage _details;
-            private Queue<Car> _cars;
-            private int _money;
+            bool isWork = true;
 
-            public CarService()
+            while (isWork)
             {
-                _money = 2000;
-                _details = new Storage();
-                _cars = new Queue<Car>();
-                СreateCar(5);
-            }
-
-            public void Work()
-            {
-                const string CommandStart = "1";
-                const string CommandExit = "2";
-
-                bool isWork = true;
-
-                while (isWork && 0 <= _cars.Count)
-                {
-                    Console.WriteLine($"\nБаланс автомастерской - {_money} монет.");
-                    Console.Write($"В мастерской {_cars.Count} машин, стоят на ремонт. " +
-                        $"\nДля их обслуживания введите {CommandStart}. " +
-                        $"\nДля завершения работы введите{CommandExit}" +
-                        $"\nДействие: ");
-                    string userInput = Console.ReadLine();
-
-                    switch (userInput)
-                    {
-                        case CommandStart:
-                            ServiceСar();
-                            break;
-
-                        case CommandExit:
-                            isWork = false;
-                            break;
-
-                        default:
-                            Console.WriteLine("Хм.. Такой команды нету.");
-                            break;
-                    }
-                }
-            }
-
-            private void СreateCar(int numberOfCar)
-            {
-                for (int i = 0; i < numberOfCar; i++)
-                {
-                    _cars.Enqueue(new Car());
-                }
-            }
-
-            private void ShowBrakdown(Car car)
-            {
-                Console.WriteLine($"У авто сломано - {car.BreakdownDetail}. ");
-                Console.WriteLine($"\nЦена за работу будет - {_details.GetRepairPrice(car)} монет.");
-            }
-
-            private void ServiceСar()
-            {
-                const string CommandRepair = "1";
-                const string CommandRefuse = "2";
-
-                Console.Clear();
-                var car = _cars.Dequeue();
-                ShowBrakdown(car);
-
-                Console.Write($"Что вы будете делать." +
-                    $"\n{CommandRepair} Ремонтировать " +
-                    $"\n{CommandRefuse} Отказать клиенту " +
-                    "\n Действие: ");
-
+                Console.WriteLine($"Добро пожаловать в зоопар.\nДля начало нажмите {CommandStart}\nВыход из зоопарка {CommandExit}");
                 string userInput = Console.ReadLine();
 
                 switch (userInput)
                 {
-                    case CommandRepair:
-                        RepairCar(car);
+                    case CommandStart:
+                        ShowAviary();
                         break;
 
-                    case CommandRefuse:
-                        DenyService();
+                    case CommandExit:
+                        isWork = false;
                         break;
 
                     default:
-                        Console.WriteLine("Такой команды нету.");
+                        Console.WriteLine("Не верный ввод");
                         break;
                 }
 
                 Console.ReadKey();
                 Console.Clear();
             }
+        }
 
-            private void RepairCar(Car car)
+        private void FillAnimas()
+        {
+            _animals.Add(new Animal("Тигры", "Р-р-р"));
+            _animals.Add(new Animal("Собаки", "Гаф"));
+            _animals.Add(new Animal("Попугай", "Кто - там"));
+            _animals.Add(new Animal("Кот", "Мяу Мяу"));
+            _animals.Add(new Animal("Крокодил", "Клоц-клоц"));
+
+        }
+
+        private void CreateAviary()
+        {
+            int min = 2;
+            int max = 7;
+
+            for (int i = 0; i < _animals.Count; i++)
             {
-                if (_details.TryRepairCar(car))
+                int count = _random.Next(min, max);
+
+                List<Animal> animals = new List<Animal>();
+
+                for (int j = 0; j < count; j++)
                 {
-                    Console.WriteLine($"Вы успешно починили автомобиль!" +
-                        $"\n И заработали {_details.GetRepairPrice(car)} монет.");
-                    _money += _details.GetRepairPrice(car);
+                    Animal animal = new Animal(_animals[i].Name, _animals[i].Voice);
+                    animals.Add(animal);
+                }
+
+                _aviaries.Add(new Aviary(animals));
+            }
+        }
+
+        private void ShowAviary()
+        {
+            int maxNumber = _aviaries.Count;
+
+            Console.WriteLine("Вольеры:");
+
+            for (int i = 0; i < _aviaries.Count; i++)
+            {
+                int number = i + 1;
+
+                Console.WriteLine($"{number}. {_aviaries[i].Name}");
+            }
+
+            Console.Write($"На какой вольер вы хотите посмотреть?: Введите номер от 1 до {maxNumber}\n");
+
+            if (int.TryParse(Console.ReadLine(), out int inputNumberAviary))
+            {
+                if (inputNumberAviary > 0 && inputNumberAviary < _aviaries.Count)
+                {
+                    _aviaries[inputNumberAviary - 1].ShowAnimals();
                 }
                 else
                 {
-                    Console.WriteLine("Вы установили не ту деталь. Клиент не доволен вашей работой. " +
-                         $"\n Вы возместитли ущерб клиенту в размере - {_details.GetRepairPrice(car)} монет.");
-                    _money -= _details.GetRepairPrice(car);
+                    Console.WriteLine("Вальера с таким номером в зоопарке нету.");
                 }
-            }
-
-            private void DenyService()
-            {
-                int penalty = 500;
-                Console.WriteLine($"Вы отказали клиенту. С вас шраф - {penalty} монет.");
-                _money -= penalty;
             }
         }
+    }
 
-        class Car
+    class Aviary
+    {
+        const string Tigger = "Тигр";
+        const string Dog = "Собака";
+        const string Parrot = "Попугай";
+        const string Cat = "Кот";
+        const string Crocodile = "Кродил";
+
+        private Random _random = new Random();
+
+        private List<Animal> _animals = new List<Animal>();
+        public string[] _listAnimals = new string[0];
+
+        public Aviary()
         {
-            private string[] _numbersBreakdown = new string[] { "2", "3",
-            "4", "5", "6" };
-            private Random _random = new Random();
-
-            public Car()
-            {
-                СreateBreakdown();
-            }
-
-            public string BreakdownDetail { get; private set; }
-
-            private void СreateBreakdown()
-            {
-                int detailId = _random.Next(0, _numbersBreakdown.Length);
-                BreakdownDetail = GetBreakdown(detailId);
-            }
-
-            private string GetBreakdown(int breakdownId)
-            {
-                const string NumberTwo = "2";
-                const string NumberThree = "3";
-                const string NumberFour = "4";
-                const string NumberаFive = "5";
-                const string NumberSix = "6";
-
-                switch (_numbersBreakdown[breakdownId])
-                {
-                    case NumberTwo:
-                        return "Колесо";
-                    case NumberThree:
-                        return "Стекло";
-                    case NumberFour:
-                        return "Фары";
-                    case NumberаFive:
-                        return "Бампер";
-                    case NumberSix:
-                        return "Замок";
-                }
-
-                return null;
-            }
+            _listAnimals = new string[] { $"{Tigger}, {Dog} , {Parrot}, {Cat}, {Crocodile}" };
         }
 
-        class Storage
+        public Aviary(List<Animal> animals)
         {
-            private List<Detail> _storage = new List<Detail>();
-            private Random _random = new Random();
-            private string[] _listDetail = new string[] { "Колесо", "Стекло", "Фары", "Бампер", "Замок" };
-
-            public Storage()
-            {
-                СreateDetail(10);
-            }
-
-            public void ShowStorage()
-            {
-                Console.WriteLine("На складе есть такие детали: ");
-
-                for (int i = 0; i < _storage.Count; i++)
-                {
-                    Console.WriteLine($"{i + 1}. Название - {_storage[i].Name}. Стоимость - {_storage[i].Cost}");
-                }
-            }
-
-            public int GetRepairPrice(Car car)
-            {
-                int repairPrice = 0;
-                int costWork = 10;
-
-                foreach (var detail in _storage)
-                {
-                    if (car.BreakdownDetail == detail.Name)
-                    {
-                        repairPrice += detail.Cost * costWork;
-                        break;
-                    }
-                }
-
-                return repairPrice;
-            }
-
-            public bool TryRepairCar(Car car)
-            {
-                ShowStorage();
-                Console.Write("\nКакую деталь вы хотитель установить: ");
-                bool isNumber = int.TryParse(Console.ReadLine(), out int inputNumberDetail);
-
-                if (isNumber == false)
-                {
-                    Console.WriteLine("Ошибка! Вы ввели не коректные данные.");
-                    return false;
-                }
-                else if (inputNumberDetail > 0 && inputNumberDetail - 1 < _storage.Count && car.BreakdownDetail == _storage[inputNumberDetail - 1].Name)
-                {
-                    int indexDetail = inputNumberDetail - 1;
-                    _storage.RemoveAt(indexDetail);
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("Кажется это на та деталь.");
-                    return false;
-                }
-            }
-
-            private void СreateDetail(int numberOfDetails)
-            {
-                for (int i = 0; i < numberOfDetails; i++)
-                {
-                    _storage.Add(GetDetail());
-                }
-            }
-
-            private Detail GetDetail()
-            {
-                const string Wheel = "Колесо";
-                const string Glass = "Стекло";
-                const string Lights = "Фары";
-                const string Bumper = "Бампер";
-                const string Lock = "Замок";
-                int detailId = _random.Next(0, _listDetail.Length);
-
-                switch (_listDetail[detailId])
-                {
-                    case Wheel:
-                        return new Detail("Колесо", 60);
-                    case Glass:
-                        return new Detail("Стекло", 80);
-                    case Lights:
-                        return new Detail("Фары", 40);
-                    case Bumper:
-                        return new Detail("Бампер", 30);
-                    case Lock:
-                        return new Detail("Замок", 50);
-                }
-
-                return null;
-            }
+            _animals = animals;
         }
 
-        class Detail
-        {
-            public Detail(string name, int cost)
-            {
-                Name = name;
-                Cost = cost;
-            }
+        public string Name => _animals[0].Name;
 
-            public string Name { get; private set; }
-            public int Cost { get; private set; }
+        public void ShowAnimals()
+        {
+            Console.WriteLine($"\nКоличиство животных в вольере - {_animals.Count}");
+
+            foreach (Animal animal in _animals)
+            {
+                Console.WriteLine($"{animal.Name}. Пол - {animal.Gender}. Голос - {animal.Voice}");
+            }
+        }
+    }
+
+    class UserUtils
+    {
+        private static Random _random = new Random();
+
+        public static int GetRandom(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+        public static int GetRandom(int max)
+        {
+            return _random.Next(max);
+        }
+
+    }
+
+    class Animal
+    {
+        private static Random _random = new Random();
+
+        public Animal(string name, string voice)
+        {
+            Name = name;
+            Choose();
+            Voice = voice;
+        }
+
+        public string Name { get; private set; }
+        public string Gender { get; private set; }
+        public string Voice { get; private set; }
+
+        public void Choose()
+        {
+            const string Female = "Самка";
+            const string Male = "Самец";
+
+            string[] arrayGender = { Female, Male };
+
+            int gender = UserUtils.GetRandom(2);
+            Gender = arrayGender[gender];
         }
     }
 }
