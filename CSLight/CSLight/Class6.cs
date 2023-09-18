d4400deaa391f0468d4e3131cd1a3cfa2cc7df4f
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2023-09-18 15:03:22 +0900

    Lesson C#
    Есть 2 списка в солдатами.
    Всех бойцов из отряда 1, у которых фамилия начинается на букву Б, требуется перевести в отряд 2.

---
+++ CSLight/CSLight/Class6.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Class6
+    {      
+            static void Main(string[] args)
+            {
+                Armies armies = new Armies();
+                armies.Work();
+            }
+        
+        class Armies
+        {
+            private List<Soldier> _firstSquad;
+            private List<Soldier> _secondSquad;
+
+            public Armies()
+            {
+                _firstSquad = new List<Soldier>();
+                _secondSquad = new List<Soldier>();
+                _firstSquad.Add(new Soldier("Собакин", "Рядовой"));
+                _firstSquad.Add(new Soldier("Иванов", "Рядовой"));
+                _firstSquad.Add(new Soldier("Белиев", "Сержант"));
+                _firstSquad.Add(new Soldier("Белогородов", "Сержант"));
+                _firstSquad.Add(new Soldier("Носков", "Старший сержант"));
+                _secondSquad.Add(new Soldier("Соболев", "Рядовой"));
+                _secondSquad.Add(new Soldier("Кудрявцев", "Рядовой"));
+            }
+
+            public void Work()
+            {
+                Console.WriteLine("Отряд 1");
+                ShowSoldiersInfo(_firstSquad);
+
+                Console.WriteLine("Отряд 2");
+                ShowSoldiersInfo(_secondSquad);
+
+                MoveSelectedSoldiers();
+
+                Console.WriteLine("\nОтряд 1");
+                ShowSoldiersInfo(_firstSquad);
+
+                Console.WriteLine("Отряд 2");
+                ShowSoldiersInfo(_secondSquad);
+            }
+
+            private void MoveSelectedSoldiers()
+            {
+                var soldiers = _firstSquad.Where(soldier => soldier.Name.Contains("Б"));
+                _secondSquad = _secondSquad.Union(soldiers).ToList();
+                _firstSquad = _firstSquad.Except(soldiers).ToList();
+            }
+
+            private void ShowSoldiersInfo(List<Soldier> soldiers)
+            {
+                foreach (var soldier in soldiers)
+                {
+                    soldier.ShowSolider();
+                }
+            }
+        }
+
+        class Soldier
+        {
+            public Soldier(string name, string title)
+            {
+                Name = name;
+                Title = title;
+            }
+
+            public string Name { get; private set; }
+            public string Title { get; private set; }
+
+            public void ShowSolider()
+            {
+                Console.WriteLine($"Name: {Name}. Title {Title}.");
+            }
+        }
+    }
+}
